01dd672e73730ee7cc00c0c2c282caf60f32a47d
parag-radadiya <parag.180410107091@gmail.com>
2025-12-15 23:09:57 +0530

    fix sidebar

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "https://https://nymph-be.vercel.app/api",
+    baseURL: "https://nymph-be.vercel.app/api",
 });
 
 instance.interceptors.request.use((config) => {
